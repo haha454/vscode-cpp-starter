a4300af9e4a8c57f5226193f9f3ff9674efd84b8
thread.start() <weitony111@hotmail.com>
2019-02-12 20:59:23 +0800

    Replace bits/stdc++.h with individual headers

--- _starter.cc
+++ _starter.cc
@@ -1,27 +1,44 @@
-#include <bits/stdc++.h>
-
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <complex>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <queue>
+#include <list>
+#include <map>
+#include <set>
 using namespace std;
- 
+
 typedef long long ll;
 typedef long double ld;
 
 typedef vector<int> vi;
 typedef vector<ll> vl;
 
-#define FOR(i, a, b) for (int i=a; i<=(b); i++)
-#define F0R(i, a) for (int i=0; i<=(a); i++)
-#define FORd(i,a,b) for (int i = (b); i >= a; i--)
-#define F0Rd(i,a) for (int i = (a); i >= 0; i--)
+#define FOR(i, a, b) for (int i = a; i <= (b); i++)
+#define F0R(i, a) for (int i = 0; i <= (a); i++)
+#define FORd(i, a, b) for (int i = (b); i >= a; i--)
+#define F0Rd(i, a) for (int i = (a); i >= 0; i--)
 
 #define sz(x) (int)(x).size()
 #define all(x) x.begin(), x.end()
 
-const double PI = 4*atan(1);
+const double PI = 4 * atan(1);
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << n;
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        cout << "Case #" << i << ": ";
+        cout << endl;
+    }
     return 0;
 }
