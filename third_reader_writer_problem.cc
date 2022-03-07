ff6cedece276f60fdd5db81e0fe93005ff0d85cd
thread start <zunhao.wei@gmail.com>
2022-03-07 15:31:38 +0800

    Add FairSemaphore in the third_reader_writer_problem

--- third_reader_writer_problem.cc
+++ third_reader_writer_problem.cc
@@ -1,12 +1,40 @@
 using namespace std;
 
+class FairSemaphore
+{
+private:
+    conditional_variable cv;
+    mutex mx;
+    long long qhead, qtail;
+
+public:
+    FairSemaphore() : qhead(0), qtail(0)
+    {
+    }
+
+    void acquire()
+    {
+        unique_lock<mutex> lk(mx);
+        long long cur = qtail++;
+        cv.wait(mx, [&cur, this]()
+                { return cur == this->qhead; });
+    }
+
+    void release()
+    {
+        lock_guard<mutex> lk(mx);
+        qhead++;
+        cv.notify_all();
+    }
+};
+
 // starve reader
 class RWLock
 {
 private:
     mutex rmtx;
     binary_semaphore resource;
-    FairBinarySemaphore fbs;
+    FairSemaphore fs;
     int rctr;
 
 public:
