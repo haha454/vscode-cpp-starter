78eb424791f2186a2ddbfc4bbed65a9131a5b2b3
Zunhao Wei <zunhao.wei@shopee.com>
2022-03-30 21:44:16 +0800

    Update three_way_partition

--- three_way_paritition_k_smallest_elements.cc
+++ three_way_paritition_k_smallest_elements.cc
@@ -1,31 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void partition(vector<int>& a, int l, int r, int& p1, int& p2){
-    if (l == r)
+/*
+Use a[l] as the pivot and partition the array [l, r) into three parts; returns pair: <el, er>
+where [l, el) is the smaller part
+[el, er) is the equal part
+[er, r) is the greater part
+*/
+pair<int, int> partition(vector<int> &a, int l, int r)
+{
+    assert(l <= r);
+    int pivot = a[l];
+    int i = l;
+    while (i < r)
     {
-        p1 = p2 = l;
-        return;
-    }
-    int i = l + 1;
-    while (i <= r){
-        if (a[i] > a[l])
+        if (a[i] > pivot)
         {
-            swap(a[i], a[r]);
             r--;
+            swap(a[i], a[r]);
         }
-        else if (a[i] < a[l])
+        else if (a[i] < pivot)
         {
             swap(a[i], a[l]);
             i++;
             l++;
         }
         else
+        {
             i++;
+        }
     }
-    p1 = l;
-    p2 = r;
+    return make_pair(l, r);
 }
 
 int main()
@@ -36,18 +42,23 @@ int main()
     vector<int> a = vector<int>(istream_iterator<int>(iss), istream_iterator<int>());
     int k;
     cin >> k;
-    int p1 = 0, p2 = -1, l = 0, r = a.size() - 1;
-    while (k < p1 || k > p2) {
-        partition(a, l, r, p1, p2);
-        if (k < p1)
-            r = p1 - 1;
-        if (k > p2)
-            l = p2 + 1;
+
+    int l = 0, r = a.size();
+    while (true)
+    {
+        auto [el, er] = partition(a, l, r);
+        if (k < el)
+            r = el;
+        else if (k > er)
+            l = er + 1;
+        else
+        {
+            break;
+        }
     }
 
-    for (auto & v : a)
+    for (auto const &v : a)
         printf("%d ", v);
-    printf("\n%d %d\n", p1, p2);
-    
+
     return 0;
 }
